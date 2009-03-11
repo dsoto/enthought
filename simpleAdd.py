a42eaaf13c80d2a73d952e742b9809bdaf9fa9d4
Daniel Soto <danielrsoto@gmail.com>
2009-03-10 17:46:37 -0700

    cleaning up

--- simpleAdd.py
+++ simpleAdd.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-from enthought.traits.api import Int, HasTraits, Button
-from enthought.traits.ui.api import View, Item
-from enthought.traits.ui.menu import OKButton, CancelButton
+from enthought.traits.api import Int, HasTraits
+from enthought.traits.ui.api import View
+from enthought.traits.ui.menu import OKButton
 
 # TODO : use separate handler class
 
@@ -10,15 +10,11 @@ class mathBox(HasTraits):
 	a = Int
 	b = Int
 	c = Int
-	mult = Button()
 
 	def __init__(self,x,y):
 		self.a = x
 		self.b = y
 		self.c = x + y
-
-	def _mult_fired(self):
-		self.c = self.a * self.b
 
 	def _a_changed(self):
 		self.c = self.a + self.b
@@ -27,9 +23,7 @@ class mathBox(HasTraits):
 		self.c = self.a + self.b
 
 	view = View('a','b','c',
-	            Item('mult', show_label=False),
-	            buttons = [OKButton, CancelButton])
+	            buttons = [OKButton])
 
-for i in range(1,4):
-	myMathBox = mathBox(i,i+1)
-	myMathBox.configure_traits()
+myMathBox = mathBox(1,2)
+myMathBox.configure_traits()
