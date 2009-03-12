49be005246cd1b3d524a3dde176131bf13c78d3e
Daniel Soto <danielrsoto@gmail.com>
2009-03-12 16:27:24 -0700

    moving add function into model

--- add.py
+++ add.py
@@ -4,8 +4,6 @@ from enthought.traits.api import Int, HasTraits, on_trait_change
 from enthought.traits.ui.api import View
 from enthought.traits.ui.menu import OKButton
 
-# TODO : use separate handler class
-
 class mathBox(HasTraits):
 	a = Int
 	b = Int
@@ -13,14 +11,16 @@ class mathBox(HasTraits):
 
 	def __init__(self,x,y):
 		super(mathBox,self).__init__()
-
 		self.a = x
 		self.b = y
 		self.c = x + y
+	
+	def add(self):
+		self.c = self.a + self.b
 
 	@on_trait_change('a,b')
 	def update(self, name, value):
-		self.c = self.a + self.b
+		self.add()
 
 	view = View('a','b','c',
 	            buttons = [OKButton])
